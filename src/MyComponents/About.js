e91ceeeb1fba7062f06df913b62d12d483a760f0
Rishabh <gopu.shukla.786@gmail.com>
2022-07-09 13:27:02 +0530

    First Commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div className='container'>
+            <strong>Hi I'm Rishabh Shukla</strong>
+            <p>So here I am going to tell you a secret about me that besides of being a tech enthusiast I am also a foodie person. I love to watch movies and also found of listening music.</p>
+        </div>
+    )
+}
---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Footer = () => {
+
+    return (
+        <footer className="fixed-bottom bg-dark text-light text-center text-lg-start">
+            <div className="text-center p-3" >
+                © 2022 Copyright :
+                <span className="text-light"> Rishabh Shukla</span>
+            </div>
+        </footer>
+    )
+}
